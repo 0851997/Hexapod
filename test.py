6773b3aa0359a1962ef5e5dc826600bdb1f102f4
DESKTOP-33RVDNI\Robert <0851997@hr.nl>
2019-03-29 00:42:29 +0100

    Serial communication test code

---
+++ test.py
@@ -0,0 +1,17 @@
+import Adafruit_BBIO.UART as UART
+import serial
+from time import sleep
+
+UART.setup("UART1")
+
+ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
+ser.close()
+ser.open()
+if ser.isOpen():
+    while(True):
+        print ("Serial is open!")
+        ser.write("#0 P500\r".encode())
+        sleep(1)
+        ser.write("#0 P2500\r".encode())
+        sleep(1)
+ser.close()
