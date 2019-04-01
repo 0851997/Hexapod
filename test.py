5d137f67dc43cf3c36c22469949798a359cc0586
BitsNByt3s <jul_daniel@hotmail.com>
2019-04-01 12:06:16 +0200

    Conf coxa servos

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ if ser.isOpen():
     while(True):
         print ("Serial is open!")
         ser.write("#0 P500\r".encode())
-        sleep(1)
-        ser.write("#0 P2500\r".encode())
-        sleep(1)
+        #sleep(1)
+        #ser.write("#0 P2500\r".encode())
+        #sleep(1)
 ser.close()
