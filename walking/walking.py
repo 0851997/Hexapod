bb96e0030ac824f8e06ca2dd7ede46691f4271ba
MstfH <mo.mh43@gmail.com>
2019-04-03 12:06:59 +0200

    a

--- walking/walking.py
+++ walking/walking.py
@@ -13,36 +13,36 @@ def tripodWalking(time, uart, portl, rate):
         ser.write(stand.encode())
         sleep(slp)
         i = 0
-            while(True):
-                #up one side A port 0,13,24 gate 1
-                upRight="#25P944#26P1056#24P1300#14P2056#1P2056#15P1944#2P1944#13P1578#0P1922T{}\r".format(time)
-                ser.write(upRight.encode())
+        while(True):
+            #up one side A port 0,13,24 gate 1
+            upRight="#25P944#26P1056#24P1300#14P2056#1P2056#15P1944#2P1944#13P1578#0P1922T{}\r".format(time)
+            ser.write(upRight.encode())
+            sleep(slp)
+            #down one side A
+            downRight="#26P1166#25P1333#24P1300#13P1578#0P1922#1P1666#14P1666#15P1833#2P1833T{}\r".format(time)
+            ser.write(downRight.encode())
+            sleep(slp)
+            #forward A
+            moveRight="#29P1922#24P1500#16P1478#0P1722#8P1300#13P1278T{}\r".format(time)
+            ser.write(moveRight.encode())
+            sleep(slp)
+            #up other side A port 8,16,29 gate 2
+            upLeft="#30P944#17P944#18P1056#31P1056#29P1422#16P1078#9P2056#10P1944#8P1800T{}\r".format(time)
+            ser.write(upLeft.encode())
+            sleep(slp)
+            #down other side A
+            downLeft="#31P1166#18P1166#30P1333#17P1333#29P1422#16P1078#8P1800#9P1666#10P1833T{}\r".format(time)
+            ser.write(downLeft.encode())
+            sleep(slp)
+            #forward A
+            moveLeft="#29P1722#24P1700#16P1278#13P1078#8P1500#0P1522T{}\r".format(time)
+            ser.write(moveLeft.encode())
+            sleep(slp)
+            i=i+1
+            if i == 3:
+                ser.write(stand.encode())
                 sleep(slp)
-                #down one side A
-                downRight="#26P1166#25P1333#24P1300#13P1578#0P1922#1P1666#14P1666#15P1833#2P1833T{}\r".format(time)
-                ser.write(downRight.encode())
-                sleep(slp)
-                #forward A
-                moveRight="#29P1922#24P1500#16P1478#0P1722#8P1300#13P1278T{}\r".format(time)
-                ser.write(moveRight.encode())
-                sleep(slp)
-                #up other side A port 8,16,29 gate 2
-                upLeft="#30P944#17P944#18P1056#31P1056#29P1422#16P1078#9P2056#10P1944#8P1800T{}\r".format(time)
-                ser.write(upLeft.encode())
-                sleep(slp)
-                #down other side A
-                downLeft="#31P1166#18P1166#30P1333#17P1333#29P1422#16P1078#8P1800#9P1666#10P1833T{}\r".format(time)
-                ser.write(downLeft.encode())
-                sleep(slp)
-                #forward A
-                moveLeft="#29P1722#24P1700#16P1278#13P1078#8P1500#0P1522T{}\r".format(time)
-                ser.write(moveLeft.encode())
-                sleep(slp)
-                i=i+1
-                if i == 3:
-                    ser.write(stand.encode())
-                    sleep(slp)
-                    i=0
+                i=0
     ser.close()
 
 def reverse():
