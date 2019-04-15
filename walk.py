a698aff636014b7468f53df6d8fcb426bf7bf408
MstfH <mo.mh43@gmail.com>
2019-04-15 12:16:12 +0200

    try2

--- walk.py
+++ walk.py
@@ -15,28 +15,28 @@ if ser.isOpen():
     while(True):
         #lynx 0
         ser.write("#29P1722#8P1500#16P1278#14P2056#1P2056#25P944#13P1278#0P1722#24P1500T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 1
         ser.write("#29P1522#8P1700#16P1078#14P1861#1P1861#25P1139#131578#0P1922#24P1300T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 2
         ser.write("#14P1666#1P1666#25P1333T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 3
         ser.write("#30P1139#9P1861#17P1139T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 4
         ser.write("#30P944#9P2056#17P944#29P1722#8P1500#16P1278#13P1278#0P1722#24P1500T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 5
         ser.write("#30P1139#9P1861#17P1139#29P1922#8P1300#16P1478#13P1078#0P1522#24P1700T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 6
         ser.write("#30P1333#9P1666#17P1333T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #lynx 7
         ser.write("#14P1861#1P1861#25P1139T50\r".encode())
-        sleep(0.05)
+        sleep(0.1)
         #B V=14,1,25  H=13,0,24
         #A V=30,9,17  H=29,8,16
         '''#up one side A port 0,13,24 gate B
