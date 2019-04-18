c285cb428424b996a6036087c37ff658d0385763
MstfH <mo.mh43@gmail.com>
2019-04-18 13:32:51 +0200

    Last comment

--- walking/tripodgate.py
+++ walking/tripodgate.py
@@ -9,6 +9,7 @@ def tripodWalking(time, uart, portl, rate):
     ser.close()
     ser.open()
     if ser.isOpen():
+        #stable stance
         ser.write("#31P1055#26P1055#18P1055#30P1277#25P1277#17P1277#29P1722#24P1500#16P1278#13P1278#8P1500#0P1722#1P1722#9P1722#14P1722#2P1944#10P1944#15P1944T200\r".encode())
         sleep(0.5)
         while(True):
