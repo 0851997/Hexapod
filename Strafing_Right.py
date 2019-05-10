e045a5e59bb72f2c070ce4641b57dc1ab84c50df
Froztbyt3 <Jul_daniel@hotmail.com>
2019-05-10 11:59:51 +0200

    3

--- Strafing_Right.py
+++ Strafing_Right.py
@@ -21,10 +21,10 @@ if ser.isOpen():
         ser.write("#31P855#30P1277#18P855#17P1277 #9P1522#10P1944 T50\r".encode())
         sleep(0.05)
         #lynxmotion guide 2 in README
-        ser.write("#31P1055#30P1477#26P1255#25P1277#18P1055#17P1477 #14P1722#15P1744#9P1722#10P2144#1P1722#2P1744 T50\r".encode())
+        ser.write("#31P1055#30P1477#26P1255#25P1277#18P1055#17P1477 #14P1522#15P1744#9P1722#10P2144#1P1522#2P1744 T50\r".encode())
         sleep(0.05)
         #lynxmotion guide 3 in README
-        ser.write("#26P855#25P1277 #14P1522#15P1944#1P1522#2P1944 T50\r".encode())
+        ser.write("#26P855#25P1277 #14P1322#15P1744#1P1322#2P1744 T50\r".encode())
         sleep(0.05)
         #lynxmotion guide 4 in README
         ser.write("#31P1255#30P1277#26P1055#25P1477#18P1255#17P1277 #14P1722#15P2144#9P1722#10P1744#1P1722#2P2144 T50\r".encode())
