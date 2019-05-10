a1c75b2cdc92beca6a5470e0630acff6334ab1ab
Froztbyt3 <Jul_daniel@hotmail.com>
2019-05-10 13:22:43 +0200

    7

--- Strafing_Right.py
+++ Strafing_Right.py
@@ -15,19 +15,19 @@ if ser.isOpen():
         #B Vertical Ports=14,1,25  Horizontal Ports=13,0,24  Corresponding legs: Right Front Leg, Right Rear Leg, Left Center Leg
         #A Vertical Ports=30,17,9  Horizontal Ports=29,16,8  Corresponding legs: Left Front Leg, Left Rear Leg, Right Center Leg
         #lynxmotion guide 0 in README
-        ser.write("#31P1255#30P1277#26P1055#25P1477#18P1255#17P1277 #14P1722#15P2144#9P1722#10P1744#1P1722#2P2144 T70\r".encode())
+        ser.write("#31P1255#30P1277#26P1055#25P1477#18P1255#17P1277 #14P1722#15P2144#9P1722#10P1744#1P1722#2P2144 T100\r".encode())
         sleep(0.05)
         #lynxmotion guide 1 in README
-        ser.write("#31P855#30P1277#18P855#17P1277 #9P1522#10P1944 T70\r".encode())
+        ser.write("#31P855#30P1277#18P855#17P1277 #9P1522#10P1944 T100\r".encode())
         sleep(0.05)
         #lynxmotion guide 2 in README
-        ser.write("#31P1055#30P1477#26P1255#25P1277#18P1055#17P1477 #14P1722#15P1744#9P1722#10P2144#1P1722#2P1744 T70\r".encode())
+        ser.write("#31P1055#30P1477#26P1255#25P1277#18P1055#17P1477 #14P1722#15P1744#9P1722#10P2144#1P1722#2P1744 T100\r".encode())
         sleep(0.05)
         #lynxmotion guide 3 in README
-        ser.write("#26P855#25P1277 #14P1522#15P1744#1P1522#2P1744 T70\r".encode())
+        ser.write("#26P855#25P1277 #14P1522#15P1744#1P1522#2P1744 T100\r".encode())
         sleep(0.05)
         #lynxmotion guide 4 in README
-        ser.write("#31P1255#30P1277#26P1055#25P1477#18P1255#17P1277 #14P1722#15P2144#9P1722#10P1744#1P1722#2P2144 T70\r".encode())
+        ser.write("#31P1255#30P1277#26P1055#25P1477#18P1255#17P1277 #14P1722#15P2144#9P1722#10P1744#1P1722#2P2144 T100\r".encode())
         sleep(0.05)
 ser.close()
 
