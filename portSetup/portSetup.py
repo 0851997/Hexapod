2205fb24f5fe2ecb88a1b9e1fa1b751059548e8d
MstfH <mo.mh43@gmail.com>
2019-05-10 11:12:40 +0200

    hope no faults

--- portSetup/portSetup.py
+++ portSetup/portSetup.py
@@ -9,6 +9,8 @@ class Ports():
     def setPort(self, uart, portl, rate):
         UART.setup(uart)
         self.ser = serial.Serial(port = portl, baudrate=rate)
+        self.ser.close()
+        self.ser.open()
 
     def getPort(self):
         return self.ser
