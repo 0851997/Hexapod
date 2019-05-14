2b124214974567dc9fa052a83d1764958ac6211a
Froztbyt3 <Jul_daniel@hotmail.com>
2019-05-14 14:05:24 +0200

    test

--- Strafing_Right.py
+++ Strafing_Right.py
@@ -37,19 +37,19 @@ if ser.isOpen():
         ser.write("#26P1255#25P1277 #14P1722#15P1744#1P1722#2P1744 T400\r".encode())
         sleep(0.40)
         # State 1: gait B duwt robot naar rechts
-        ser.write("#31P1055#30P1477#18P1055#17P1477 #9P1622#10P1944 T400\r".encode())
+        ser.write("#31P1055#30P1477#18P1055#17P1477 #9P1722#10P2144 T400\r".encode())
         sleep(0.40)
         # State 2: gait A zet poten op een stabiele positie
-        ser.write("#26P1055#25P1177 #14P1522#15P1944#1P15S22#2P1944 T400\r".encode())
+        ser.write("#26P855#25P1277 #14P1522#15P1944#1P1522#2P1944 T400\r".encode())
         sleep(0.40)
         # State 3: gait B poten gaat omhoog
         ser.write("#31P1255#30P1277#18P1255#17P1277 #9P1722#10P1744 T400\r".encode())
         sleep(0.40)
         # State 4: gait A trek robot naar rechts
-        ser.write("#26P1055#25P1477 #14P1722#15P2044#1P1722#2P2044 T400\r".encode())
+        ser.write("#26P1055#25P1477 #14P1722#15P2144#1P1722#2P2144 T400\r".encode())
         sleep(0.40)
         # State 5: gait B zet poten bij een stabiele positie
-        ser.write("#31P955#30P1277#18P955#17P1277 #9P1522#10P2144 T400\r".encode())
+        ser.write("#31P855#30P1277#18P855#17P1277 #9P1522#10P1944 T400\r".encode())
         sleep(0.40)
 ser.close()
 
