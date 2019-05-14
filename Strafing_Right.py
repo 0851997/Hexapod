f103bf934b04b1903710b6801862660bbad67202
Froztbyt3 <Jul_daniel@hotmail.com>
2019-05-14 12:57:42 +0200

    rear adjustment

--- Strafing_Right.py
+++ Strafing_Right.py
@@ -37,10 +37,10 @@ if ser.isOpen():
         ser.write("#26P1255#25P1277 #14P1722#15P1744#1P1722#2P1744 T400\r".encode())
         sleep(0.40)
         # State 1: gait B duwt robot naar rechts
-        ser.write("#31P1055#30P1477#18P1055#17P1677 #9P1622#10P1944 T400\r".encode())
+        ser.write("#31P1055#30P1477#18P1055#17P1477 #9P1622#10P1944 T400\r".encode())
         sleep(0.40)
         # State 2: gait A zet poten op een stabiele positie
-        ser.write("#26P1055#25P1177 #14P1522#15P1944#1P1322#2P1944 T400\r".encode())
+        ser.write("#26P1055#25P1177 #14P1522#15P1944#1P15S22#2P1944 T400\r".encode())
         sleep(0.40)
         # State 3: gait B poten gaat omhoog
         ser.write("#31P1255#30P1277#18P1255#17P1277 #9P1722#10P1744 T400\r".encode())
