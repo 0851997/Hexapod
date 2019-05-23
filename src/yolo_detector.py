180863b82a0bd339faca04ec09de21f23d760524
Remco van Gorsel <r.gorsel98@upcmail.nl>
2019-05-23 08:44:44 +0200

    Adding calculations for the width and height of the found person and the distance between the center and the border

--- src/yolo_detector.py
+++ src/yolo_detector.py
@@ -9,6 +9,7 @@ import time
 import cv2
 import os
 
+# Parameters to run this program
 ap = argparse.ArgumentParser()
 ap.add_argument("-y", "--yolo", required=True,
 	help="base path to YOLO directory")
@@ -39,11 +40,11 @@ time.sleep(1.0)
 
 while True:
 	frame = vs.read()
-
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
 		print("[INFO] Height:", H, "Width:", W)
 
+	# Making a blob and putting it through the layers of the yolo detection
 	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
 		swapRB=True, crop=False)
 	net.setInput(blob)
@@ -61,7 +62,7 @@ while True:
 			classID = np.argmax(scores)
 			confidence = scores[classID]
 
-			if LABELS[classID] in 'person':
+			if LABELS[classID] in 'person': # Filter out off all the detected things to only persons
 				if confidence > args["confidence"]:
 					box = detection[0:4] * np.array([W, H, W, H])
 					(centerX, centerY, width, height) = box.astype("int")
@@ -76,16 +77,22 @@ while True:
 	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
 		args["threshold"])
 
-	if len(idxs) > 0:
+	if len(idxs) > 0: # If there is more than 0 persons detected draw it on the screen
 		for i in idxs.flatten():
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
 
 			color = [int(c) for c in COLORS[classIDs[i]]]
 			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			dimensionsRectangle = (((x+w)-x), ((y+h)-y))
 			rectCenter = (int((x+x+w)/2), int((y+y+h)/2))
+			distanceCenterToBorder = (dimensionsRectangle[0]/2, dimensionsRectangle[1]/2)
 			cv2.circle(frame, rectCenter, 5, (0, 0, 210), 5)
-			print("Center coordinates of the found person:", rectCenter) #<---- rectCenter is the center point of the found person
+			print("")
+			print("[INFO] Width and Height of the found person   			:", dimensionsRectangle) 		#<---- dimensionsRectangle is the width and height of the box around the person
+			print("[INFO] Center coordinates of the found person 			:", rectCenter) 				#<---- rectCenter is the center point of the found person
+			print("[INFO] From the center to the border of the rectangle 	:", distanceCenterToBorder) 	#<---- distanceCenterToBorder is the distance between the found person center and the drawn border around it
+			print("")
 			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
 				confidences[i])
 			cv2.putText(frame, text, (x, y - 5),
