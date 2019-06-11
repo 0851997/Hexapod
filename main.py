485e7c7c10cb6b79059ff88346d99e9a6390b0b4
Demo User <debian@beaglebone.localdomain>
2019-06-11 21:35:07 +0000

    main commited

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ try:
                     walking.tripodWalking(config, 0.05)
                 elif mode == 'e':
                     walking.strafeRight(config, 0.05)
-                    break
                 elif mode == 'a':
                     walking.turnLeft(config)
                 elif mode == 's':
