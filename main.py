cc218e306ef66243995a05bfbae70229d2484e4f
MstfH <mo.mh43@gmail.com>
2019-06-17 13:56:35 +0200

    typo

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ connection = server.Server('192.168.43.189',10000,ready)
 mythread = threading.Thread(target=connection.connect)
 mythread.start()
 ready.wait()
-thread = threading.Thread(target=connection.start)
+thread = threading.Thread(target=connection.run)
 thread.start()
 
 try:
