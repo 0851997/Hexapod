a173bc401cbc46b59f5a5cd0bd1a9a1b2e21477e
MstfH <mo.mh43@gmail.com>
2019-06-17 14:11:20 +0200

    no send

--- yoloServer/yolo_pickle_server.py
+++ yoloServer/yolo_pickle_server.py
@@ -31,4 +31,4 @@ class Server:
                 print(self.dimensionRectangle, self.rectCenter,self.distanceCenterToBorder)
                 self.values = self.dimensionRectangleWidth, self.dimensionRectangleHeight, self.rectCenterWidth, self.rectCenterHeight, self.distancBorderWidth, self.distanceBorderHeight
                 print(self.values)
-                self.conn.send(self.data)
+                #self.conn.send(self.data)
