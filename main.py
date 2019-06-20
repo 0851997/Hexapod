eb02abf342d2281e932af140ac5b0a1ec4df9c68
DESKTOP-33RVDNI\Robert <0851997@hr.nl>
2019-06-20 15:04:35 +0200

    turnRight and turnLeft remake of movement

--- main.py
+++ main.py
@@ -27,37 +27,51 @@ try:
     if config.serialConn.isOpen():
         standing.stableStance(config, 0.2)
         while(True):
-            #turnLeftBoudaries = 0 - 85
-            #strafeLeftBoundary = 85 - 224
-            #forwardBackwardBoundary = 224 - 416
-            #strafeRightBoundary = 416 - 555
-            #turnRightBoundary = 555 - 640
-            
-            if (connection.rectCenterWidth < 112 and connection.rectCenterWidth > 0):
-                walking.turnLeft(config, 0.2)
-            elif (connection.rectCenterWidth < 224 and connection.rectCenterWidth > 112):
-                walking.strafeLeft(config, 0.2)
-            elif (connection.rectCenterWidth < 406 and connection.rectCenterWidth > 278):
-                #total 90 - 176
-                #front 90 - 119
-                if connection.dimensionRectangleWidth > 90 and connection.dimensionRectangleWidth < 119:
-                    walking.tripodWalking(config, 0.2)
-                #stand 119 - 147
-                elif connection.dimensionRectangleWidth > 119 and connection.dimensionRectangleWidth < 147:
+            mode = input("Press a key for movement command: ")
+            for i in range(3):
+                if mode == 'q':
+                    walking.strafeLeft(config, 0.5)
+                elif mode == 'w':
+                    walking.tripodWalking(config, 0.5)
+                elif mode == 'e':
+                    walking.strafeRight(config, 0.5)
+                elif mode == 'a':
+                    #we are testing this now
+                    walking.turnLeft(config, 3)
+                elif mode == 's':
+                    walking.reverse(config, 0.5)
+                elif mode == 'd':
+                    #we are testing this now
+                    walking.turnRight(config, 3)
+                elif mode == 'z':
                     standing.stableStance(config, 0.2)
-                #back 147 - 176
-                elif connection.dimensionRectangleWidth > 147 and connection.dimensionRectangleWidth < 176:
-                    walking.reverse(config, 0.2)
-            elif (connection.rectCenterWidth < 512 and connection.rectCenterWidth > 406):
-                walking.strafeRight(config, 0.2)
-            elif (connection.rectCenterWidth < 640 and connection.rectCenterWidth > 512):
-                walking.turnRight(config, 0.2)
-            elif connection.rectCenterWidth < 0 or connection.rectCenterWidth > 640:
-                standing.stableStance(config, 0.2)
-            else:
-                standing.stableStance(config, 0.2)
-
-            time.sleep(0.5)
+                    break
+                elif mode == 'x':
+                    standing.sit(config)
+                    break
+#             if (connection.rectCenterWidth > 0 and connection.rectCenterWidth < 213):
+#                 walking.turnLeft(config, 0.05)
+#             #elif (connection.rectCenterWidth < 224 and connection.rectCenterWidth > 112):
+#             #   walking.strafeLeft(config, 0.2)
+#             elif (connection.rectCenterWidth > 213 and connection.rectCenterWidth < 416):
+#                 #total 90 - 176
+#                 #front 90 - 119
+#                 if connection.dimensionRectangleWidth > 90 and connection.dimensionRectangleWidth < 119:
+#                     walking.tripodWalking(config, 0.05)
+#                 #stand 119 - 147
+#                 elif connection.dimensionRectangleWidth > 119 and connection.dimensionRectangleWidth < 147:
+#                     standing.stableStance(config, 0.2)
+#                 #back 147 - 176
+#                 elif connection.dimensionRectangleWidth > 147 and connection.dimensionRectangleWidth < 176:
+#                     walking.reverse(config, 0.05)
+#             #elif (connection.rectCenterWidth < 512 and connection.rectCenterWidth > 406):
+#             #    walking.strafeRight(config, 0.2)
+#             elif (connection.rectCenterWidth > 416 and connection.rectCenterWidth < 640):
+#                 walking.turnRight(config, 0.05)
+#             elif connection.rectCenterWidth < 0 or connection.rectCenterWidth > 640:
+#                 standing.stableStance(config, 0.2)
+#             else:
+#                 standing.stableStance(config, 0.2)
 except KeyboardInterrupt:
     standing.sit(config)
     config.serialConn.close()
