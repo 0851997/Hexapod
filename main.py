0dd5a395b2cb870367a02cc0fe2dc5b8fb716c2b
DESKTOP-33RVDNI\Robert <0851997@hr.nl>
2019-06-24 14:41:16 +0200

    change combined pickle code, detection commented out only movement check

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ config.initializePorts()
 config.serialConn.close()
 config.serialConn.open()
 
-ready = threading.Event()
-connection = server.Server('192.168.43.5',8787,ready)
-mythread = threading.Thread(target=connection.connect)
-mythread.start()
-ready.wait()
-thread = threading.Thread(target=connection.run)
-thread.start()
+# ready = threading.Event()
+# connection = server.Server('192.168.43.5',8787,ready)
+# mythread = threading.Thread(target=connection.connect)
+# mythread.start()
+# ready.wait()
+# thread = threading.Thread(target=connection.run)
+# thread.start()
 
 try:
     if config.serialConn.isOpen():
@@ -29,47 +29,67 @@ try:
         
         #Loop for main program
         while(True):
-            #turnLeftBoudaries = 0 - 85
-            #strafeLeftBoundary = 85 - 224
-            #forwardBackwardBoundary = 224 - 416
-            #strafeRightBoundary = 416 - 555
-            #turnRightBoundary = 555 - 640
-            if (connection.rectCenterWidth < 112 and connection.rectCenterWidth > 0):
-                standing.stableStance(config, 0.2)
-                walking.turnLeft(config, 0.5)
-                
-            elif (connection.rectCenterWidth < 224 and connection.rectCenterWidth > 112):
-                walking.strafeLeft(config, 0.5)
-                
-            elif (connection.rectCenterWidth < 406 and connection.rectCenterWidth > 278):
-                #front 85 - 220
-                #stand 220 - 265
-                #back 265 - 615
-                if connection.dimensionRectangleWidth > 90 and connection.dimensionRectangleWidth < 119:
-                    standing.stableStance(config, 0.2)
+            mode = input("Press a key for movement command: ")
+            for i in range(3):
+                if mode == 'q':
+                    walking.strafeLeft(config, 0.5)
+                elif mode == 'w':
                     walking.tripodWalking(config, 0.5)
-                    
-                elif connection.dimensionRectangleWidth > 119 and connection.dimensionRectangleWidth < 147:
-                    standing.stableStance(config, 0.2)
-
-                elif connection.dimensionRectangleWidth > 147 and connection.dimensionRectangleWidth < 176:
-                    standing.stableStance(config, 0.2)
+                elif mode == 'e':
+                    walking.strafeRight(config, 0.5)
+                elif mode == 'a':
+                    walking.turnLeft(config, 0.5)
+                elif mode == 's':
                     walking.reverse(config, 0.5)
-                    
-            elif (connection.rectCenterWidth < 512 and connection.rectCenterWidth > 406):
-                walking.strafeRight(config, 0.5)
-
-            elif (connection.rectCenterWidth < 640 and connection.rectCenterWidth > 512):
-                standing.stableStance(config, 0.2)
-                walking.turnRight(config, 0.5)
-
-            elif connection.rectCenterWidth < 0 or connection.rectCenterWidth > 640:
-                standing.stableStance(config, 0.2)
-            
-            else:
-                standing.stableStance(config, 0.2)
-
-            time.sleep(0.5)
+                elif mode == 'd':
+                    walking.turnRight(config, 0.5)
+                elif mode == 'z':
+                    standing.stableStance(config, 0.2)
+                    break
+                elif mode == 'x':
+                    standing.sit(config)
+                    break
+#             #turnLeftBoudaries = 0 - 85
+#             #strafeLeftBoundary = 85 - 224
+#             #forwardBackwardBoundary = 224 - 416
+#             #strafeRightBoundary = 416 - 555
+#             #turnRightBoundary = 555 - 640
+#             if (connection.rectCenterWidth < 112 and connection.rectCenterWidth > 0):
+#                 standing.stableStance(config, 0.2)
+#                 walking.turnLeft(config, 0.5)
+#                 
+#             elif (connection.rectCenterWidth < 224 and connection.rectCenterWidth > 112):
+#                 walking.strafeLeft(config, 0.5)
+#                 
+#             elif (connection.rectCenterWidth < 406 and connection.rectCenterWidth > 278):
+#                 #front 85 - 220
+#                 #stand 220 - 265
+#                 #back 265 - 615
+#                 if connection.dimensionRectangleWidth > 90 and connection.dimensionRectangleWidth < 119:
+#                     standing.stableStance(config, 0.2)
+#                     walking.tripodWalking(config, 0.5)
+#                     
+#                 elif connection.dimensionRectangleWidth > 119 and connection.dimensionRectangleWidth < 147:
+#                     standing.stableStance(config, 0.2)
+# 
+#                 elif connection.dimensionRectangleWidth > 147 and connection.dimensionRectangleWidth < 176:
+#                     standing.stableStance(config, 0.2)
+#                     walking.reverse(config, 0.5)
+#                     
+#             elif (connection.rectCenterWidth < 512 and connection.rectCenterWidth > 406):
+#                 walking.strafeRight(config, 0.5)
+# 
+#             elif (connection.rectCenterWidth < 640 and connection.rectCenterWidth > 512):
+#                 standing.stableStance(config, 0.2)
+#                 walking.turnRight(config, 0.5)
+# 
+#             elif connection.rectCenterWidth < 0 or connection.rectCenterWidth > 640:
+#                 standing.stableStance(config, 0.2)
+#             
+#             else:
+#                 standing.stableStance(config, 0.2)
+# 
+#             time.sleep(0.5)
 
 except KeyboardInterrupt:
     standing.sit(config)
