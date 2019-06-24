8a37e2de5935619bd20242f37df94634be88cfce
MstfH <mo.mh43@gmail.com>
2019-06-24 14:02:09 +0200

    print STANDING!!!!!

--- standing/stableStance.py
+++ standing/stableStance.py
@@ -1,12 +1,11 @@
 from time import sleep
 
 def stableStance(config, time):
-    #stablestance
+    print("STANDING!!!!!!!!!")
     config.serialConn.write("#31P1055#26P1055#18P1055#30P1277#25P1277#17P1277#29P1722#24P1500#16P1278#13P1278#8P1500#0P1722#1P1722#9P1722#14P1722#2P1944#10P1944#15P1944T200r".encode())
     sleep(time)
 
 def sit(config):
-    #sit
     stableStance(config, 0.2)
     config.serialConn.write("#30P1139#9P1861#17P1139#14P1861#1P1861#25P1139#29P2022T500\r".encode())
     config.serialConn.write("#14P2056#1P2056#25P944#30P944#9P2056#17P944T500\r".encode())
